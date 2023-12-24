d786ec36e73911629c81797b2595874af17a1ce2
Max Ivanov <waxivanov@gmail.com>
2023-12-24 15:36:44 +0200

    feat: connect to the db

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+const start = async () => {
+  try {
+    await sequelize.authenticate(); // connect to the db
+    await sequelize.sync(); // checks the state of the database against the schema
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
