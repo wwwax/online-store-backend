c627911bdfee53b4a2def33eccce3c444356cea5
Max Ivanov <waxivanov@gmail.com>
2024-02-14 19:45:56 +0200

    feat: add device model

--- models/models.js
+++ models/models.js
@@ -35,3 +35,28 @@ const BasketDevice = sequelize.define('basket_device', {
     autoIncrement: true,
   },
 });
+
+const Device = sequelize.define('device', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  rating: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  img: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
