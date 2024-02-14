c91e5bd51f6f7ae59bb42550dd264d6f40e2beff
Max Ivanov <waxivanov@gmail.com>
2024-02-14 19:52:15 +0200

    feat: add type, brand, rating and device info models

--- models/models.js
+++ models/models.js
@@ -60,3 +60,57 @@ const Device = sequelize.define('device', {
     allowNull: false,
   },
 });
+
+const Type = sequelize.define('type', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+const Brand = sequelize.define('brand', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+});
+
+const Rating = sequelize.define('rating', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  rate: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+const DeviceInfo = sequelize.define('device_info', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
