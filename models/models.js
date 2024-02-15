5ca756c5ce1bae7dee63c156a66b5ce6d9533c65
Max Ivanov <waxivanov@gmail.com>
2024-02-15 11:54:18 +0200

    feat: add relations

--- models/models.js
+++ models/models.js
@@ -114,3 +114,57 @@ const DeviceInfo = sequelize.define('device_info', {
     allowNull: false,
   },
 });
+
+const TypeBrand = sequelize.define('type_brand', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+});
+
+// relations
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Rating);
+Rating.belongsTo(User);
+
+Basket.hasMany(BasketDevice);
+BasketDevice.belongsTo(Basket);
+
+Type.hasMany(Device);
+Device.belongsTo(Type);
+
+Brand.hasMany(Device);
+Device.belongsTo(Brand);
+
+Device.hasMany(Rating);
+Rating.belongsTo(Device);
+
+Device.hasMany(BasketDevice);
+BasketDevice.belongsTo(Device);
+
+Device.hasMany(DeviceInfo);
+DeviceInfo.belongsTo(Device);
+
+Type.belongsToMany(Brand, {
+  through: TypeBrand,
+});
+
+Brand.belongsToMany(Type, {
+  through: TypeBrand,
+});
+
+module.exports = {
+  User,
+  Basket,
+  BasketDevice,
+  Device,
+  Type,
+  Brand,
+  Rating,
+  TypeBrand,
+  DeviceInfo,
+};
