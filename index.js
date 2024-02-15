4a179e01a3acc869189e9a81f87d844312aede88
Max Ivanov <waxivanov@gmail.com>
2024-02-15 17:57:28 +0200

    feat: routes and endpoints

--- index.js
+++ index.js
@@ -3,16 +3,14 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Working!' });
-});
+app.use('/api', router);
 
 const start = async () => {
   try {
